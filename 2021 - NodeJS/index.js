7c2ac9ecf00c895e790215b3837537a33559410e
Lorenzo Dellacà <lorenzo.dellaca@mind-overflow.net>
2023-02-27 23:02:19 +0100

    Add comments

--- 2021 - NodeJS/index.js
+++ 2021 - NodeJS/index.js
@@ -1,27 +1,31 @@
 const fs = require("fs");
 
+// class used to identify single building objects
 class Building {
     constructor(x, y, latency_weight, speed_weight) {
         this.x = x;
         this.y = y;
-        this.latency_weight = latency_weight;
-        this.speed_weight = speed_weight;
+        this.latency_weight = latency_weight; // how important the latency is
+        this.speed_weight = speed_weight; // how important the speed is
     }
 }
 
+// class used to identify placeable antennas
 class Antenna {
     constructor(x, y, range, conn_speed) {
         this.x = x;
         this.y = y;
-        this.range = range;
-        this.conn_speed = conn_speed;
+        this.range = range; // how many "squares" this antenna covers
+        this.conn_speed = conn_speed; // connection speed
     }
 }
 
+// compute distance between a building and an antenna (manhattan distance)
 function computeDistance(building, antenna) {
     return (building.x - antenna.x) + (building.y - antenna.y);
 }
 
+// sorted (based on highest score) array of all antennas reachable from a specific building
 function getReachableAntennas(building) {
     let reachableAntennas = [];
 
@@ -38,10 +42,12 @@ function getReachableAntennas(building) {
     return reachableAntennas;
 }
 
+
 function computeConnectionScore(building, antenna) {
     return building.speed_weight * antenna.conn_speed - building.latency_weight * computeDistance(building, antenna);
 }
 
+// check which antenna a specific building will connect to; NULL if no nearby antenna is found
 function getBestAntenna(building) {
     let antennas = getReachableAntennas(building);
     if (antennas.length > 0) {
@@ -51,6 +57,7 @@ function getBestAntenna(building) {
     }
 }
 
+// compute specific score of a building based on the connection score; 0 if no antenna is found
 function computeBuildingScore(building) {
 
     let antenna = getBestAntenna(building);
@@ -60,7 +67,7 @@ function computeBuildingScore(building) {
     return computeConnectionScore(building, antenna);
 }
 
-
+// returns an array of all the buildings that currently have no nearby antenna to connect to
 function missingBuildingAntennas() {
 
     let missingBuildings = [];
@@ -92,15 +99,19 @@ function computeTotalScore() {
     return score + reward;
 }
 
+// get the medium point between two buildings
 function getMidPoint(buildingA, buildingB) {
 
     return {x: parseInt(((buildingA.x + buildingB.x)/2).toFixed()), y: parseInt(((buildingA.y + buildingB.y)/2).toFixed())};
 }
 
+// returns an array of all the points where we should consider placing antennas,
+// to avoid brute-forcing the whole grid and instead focus on a few specific locations.
 function getLikelyAntennaPoints() {
 
     let points = [];
 
+    // compute all medium points between buildings
     for(let i = 0; i < buildings.length; i++) {
         let buildingA = buildings[i];
 
@@ -125,6 +136,8 @@ function getLikelyAntennaPoints() {
         }
     }
 
+    // add all buildings to the list, since buildings can hold antennas themselves
+    // and are thus valid candidates
     for(let i = 0; i < buildings.length; i++) {
         let building = buildings[i];
 
@@ -153,17 +166,22 @@ function getLikelyAntennaPoints() {
 
 function getBestPointForAntenna(antenna)
 {
-
+    // todo
 }
 
+// <debug> save starting time in milliseconds since epoch
 let startTime = Date.now();
 
+// init needed variables
 let height, width;
 let buildingsAmount, availableAntennas, reward;
 let buildings = [];
 let antennas = [];
 
+// open input file
 fs.readFile("data_scenarios_a_example.in", (err, f) => {
+
+    // parse input file
     let fileString = f.toString();
     let fileLines = fileString.split("\n");
 
@@ -178,7 +196,8 @@ fs.readFile("data_scenarios_a_example.in", (err, f) => {
     availableAntennas = parseInt(buildingsInfo[1]);
     reward = parseInt(buildingsInfo[2]);
 
-    for(let index = 2; index < buildingsAmount+2; index++) //building info starts at line 2
+    for(let index = 2; index < buildingsAmount+2; index++)
+    // ^ building info starts at line 2
     {
         let buildingInfo = fileLines[index].split(" ")
         let building = new Building(parseInt(buildingInfo[0]), parseInt(buildingInfo[1]), parseInt(buildingInfo[2]), parseInt(buildingInfo[3]))
@@ -187,6 +206,7 @@ fs.readFile("data_scenarios_a_example.in", (err, f) => {
     }
 
     for(let index = 2 + buildingsAmount; index < 2 + buildingsAmount + availableAntennas - 1; index++)
+    // ^ antennas info starts after buildings
     {
         let antennaInfo = fileLines[index].split(" ");
         let antenna = new Antenna(null, null, parseInt(antennaInfo[0]), parseInt(antennaInfo[1]));
@@ -194,11 +214,13 @@ fs.readFile("data_scenarios_a_example.in", (err, f) => {
         antennas.push(antenna)
     }
 
-    // sort antennas by having the one with the highest range first
+    // sort antennas by range (highest to lowest)
     antennas.sort((a, b) => parseInt(a.range) < parseInt(b.range) ? 1 : -1);
 
+    // compute all points where we should consider placing antennas
     let likelyAntennaPoints = getLikelyAntennaPoints();
 
+    // print map legend
     console.log("-------------------------------")
     console.log("| City Map:                   |")
     console.log("| □ = empty                   |")
@@ -207,6 +229,8 @@ fs.readFile("data_scenarios_a_example.in", (err, f) => {
     console.log("| ▣ = building with antenna   |")
     console.log("| △ = possibly good position  |")
     console.log("-------------------------------\n")
+
+    // print city map
     for(let y = 0; y  < width; y++) {
         for(let x = 0; x < height; x++) {
             let found = 0;
@@ -257,11 +281,14 @@ fs.readFile("data_scenarios_a_example.in", (err, f) => {
         process.stdout.write("\n");
     }
 
+    // <debug> log places where we should consider placing antennas
     console.log("\nLikely antenna points: ");
     console.log(getLikelyAntennaPoints());
 
+    // <debug> save ending time in milliseconds since epoch
     let endTime = Date.now();
-
+    
+    // <debug> log total runtime
     console.log("\nRunning took " + (endTime-startTime) + "ms");
 })
 
